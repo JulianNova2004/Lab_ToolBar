499c7d90b251402ac96a9937a8423fc35afa04ca
Nicolas041020 <nm341510@gmail.com>
2025-04-05 21:44:48 -0500

    v2 carring

--- app/src/main/java/co/edu/unipiloto/pizzas/MainActivity.java
+++ app/src/main/java/co/edu/unipiloto/pizzas/MainActivity.java
@@ -1,7 +1,14 @@
 package co.edu.unipiloto.pizzas;
 
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+//import android.support.v7.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,13 +18,49 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences sharedPreferences;
+
+    private EditText name,race,weight,age;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
-        setSupportActionBar(toolbar);
+        ImageView add = findViewById(R.id.add);
+         name = findViewById(R.id.name);
+         race = findViewById(R.id.race);
+         weight = findViewById(R.id.weight);
+         age = findViewById(R.id.age);
+
+
+
+        //add.setVisibility(View.GONE);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void sub(View view) {
+        String nameStr = name.getText().toString();
+        String raceStr = race.getText().toString();
+        String weightStr = weight.getText().toString();
+        String ageStr = age.getText().toString();
+        sharedPreferences = getSharedPreferences("petin", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Toast.makeText(this,nameStr,Toast.LENGTH_SHORT).show();
+        editor.putString("name", nameStr);
+        editor.putString("race", raceStr);
+        editor.putString("weight", weightStr);
+        editor.putString("age", ageStr);
+        editor.apply();
+        Toast.makeText(this,"Registro exitoso",Toast.LENGTH_SHORT).show();
+
+
     }
 }
--- app/src/main/java/co/edu/unipiloto/pizzas/OrderActivity.java
+++ app/src/main/java/co/edu/unipiloto/pizzas/OrderActivity.java
@@ -1,6 +1,10 @@
 package co.edu.unipiloto.pizzas;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +20,18 @@ public class OrderActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_order);
 
+        TextView name = findViewById(R.id.name);
+        TextView race = findViewById(R.id.race);
+        TextView weight = findViewById(R.id.weight);
+        TextView age = findViewById(R.id.age);
 
+        SharedPreferences preferences = getSharedPreferences("petin", MODE_PRIVATE);
+
+        String n = preferences.getString("name","");
+        Toast.makeText(this,n,Toast.LENGTH_SHORT).show();
+        name.setText("nombre : " + preferences.getString("name",""));
+        race.setText("raza : " +preferences.getString("race",""));
+        weight.setText("weight : " + preferences.getString("weight",""));
+        age.setText("age : " + preferences.getString("age",""));
     }
 }
