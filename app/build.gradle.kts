4933124620d6a2559c2d80c74f0383e8c3e6281c
JulianNova2004 <jnovatorroledo@gmail.com>
2025-04-05 23:54:48 -0500

    Fix: dependencies errors

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,8 @@ android {
 
 dependencies {
 
-    implementation(libs.appcompat)
+    implementation("androidx.appcompat:appcompat:1.6.0")
+    //implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
--- app/src/main/java/co/edu/unipiloto/pizzas/MainActivity.java
+++ app/src/main/java/co/edu/unipiloto/pizzas/MainActivity.java
@@ -4,21 +4,29 @@ package co.edu.unipiloto.pizzas;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+
+//import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 //import android.support.v7.widget.Toolbar;
 
+//import android.support.v4.view.MenuItemCompat;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ShareActionProvider;
 import androidx.core.graphics.Insets;
+import androidx.core.view.MenuItemCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     private SharedPreferences sharedPreferences;
+    private ShareActionProvider shareActionProvider;
 
     private EditText name,race,weight,age;
 
@@ -29,12 +37,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView add = findViewById(R.id.add);
-         name = findViewById(R.id.name);
-         race = findViewById(R.id.race);
-         weight = findViewById(R.id.weight);
-         age = findViewById(R.id.age);
-
-
+        name = findViewById(R.id.name);
+        race = findViewById(R.id.race);
+        weight = findViewById(R.id.weight);
+        age = findViewById(R.id.age);
 
         //add.setVisibility(View.GONE);
         add.setOnClickListener(new View.OnClickListener() {
@@ -60,7 +66,43 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("age", ageStr);
         editor.apply();
         Toast.makeText(this,"Registro exitoso",Toast.LENGTH_SHORT).show();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        //MenuItem item = menu.findItem(R.id.action_share);
+        //shareActionProvider = new ShareActionProvider(this);
+        //item.setActionProvider(shareActionProvider);
+        //shareActionProvider = (ShareActionProvider) item.getActionProvider();
+        //MenuItem menuItem = menu.findItem(R.id.action_share);
+        //shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Want to join to register yout pet?");
+        return super.onCreateOptionsMenu(menu);
+        //return true;
+    }
 
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, text);
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(intent);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_create_order) {
+            Intent intent = new Intent(this, OrderActivity.class);
+            startActivity(intent);
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
     }
 }
